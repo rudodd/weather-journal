91b39be1dca6b5cefb2c5369679466eea198c0e4
Rustin Dodd <rustin.dodd@q2ebanking.com>
2020-04-02 07:22:10 -0500

    setup server

--- server.js
+++ server.js
@@ -2,18 +2,25 @@
 projectData = {};
 
 // Require Express to run server and routes
-
+const express = require('express');
+const app = express();
 // Start up an instance of app
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+const port = 3000;
+const server = app.listen(port, ()=> {
+  console.log(`running on localhost: ${port}`);
+});
